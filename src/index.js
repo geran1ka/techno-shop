edc97012b74de81bb4556aa9ecf1bab7683b2ae7
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-06-06 22:42:41 +0300

    day 3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,32 @@ import "./index.html";
 import "./card.html";
 import "./cart.html";
 import "./index.scss";
+
+import Swiper from "swiper";
+import { Navigation, Pagination, Thumbs, Scrollbar } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/scrollbar";
+
+const thumbSwiper = new Swiper(".card__image", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+  modules: [Scrollbar],
+});
+
+new Swiper(".card__slider-thumd", {
+  spaceBetween: 44,
+  slidesPerView: 3,
+  thumbs: {
+    swiper: thumbSwiper,
+    slideThumbActiveClass: ".card__thumb-btn_active",
+  },
+  modules: [Thumbs],
+});
+new Swiper(".recomended__carousel", {
+  spaceBetween: 30,
+  slidesPerView: 5,
+});
